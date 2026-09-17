451e1f68e355cce328164886ab7ab897aa2c32c0
Otto <prograbugs@gmail.com>
2026-09-16 22:28:39 -0600

    First Movement Advance
    Implemented movement with jump and check if on layer "Ground"

--- Waataa'a out/Assets/Scripts/Player.cs
+++ Waataa'a out/Assets/Scripts/Player.cs
@@ -1,28 +1,62 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [Header("References")]
-    private CharacterController _characterController;
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private InputActionAsset _playerInputSystem;
+    [SerializeField] private Rigidbody2D _playerRigidBody;
+    [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private InputActionAsset _playerInputActions;
 
     [Header("Movement")]
+    private InputAction _jumpAction;
     [SerializeField] private float _jumpStrength;
-    private int _xInput;
+    private InputAction _moveAction;
+    private Vector2 _moveValue;
+    private bool _onGround => Physics2D.OverlapCircle(_overlapCenter.transform.position, _radio, _ground);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("OverlapCircle")]
+    [SerializeField] private float _radio;
+    [SerializeField] private GameObject _overlapCenter;
+    [SerializeField] private LayerMask _ground;
+
+    private void OnEnable()
     {
-        _characterController = GetComponent<CharacterController>();
+        _playerInputActions.FindActionMap("Player").Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-
+        _playerInputActions.FindActionMap("Player").Disable();
     }
 
-    private void CharacterMovement(int _xInput, float _jumpStrength)
+    private void Awake()
     {
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _moveAction = InputSystem.actions.FindAction("Move");
 
+        _playerAnimator = GetComponent<Animator>();
+        _playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _moveValue = _moveAction.ReadValue<Vector2>();
+        if(_jumpAction.WasPressedThisFrame() && _moveAction.IsPressed() && _onGround)
+        {
+            Jump();
+        }
+    }
+    
+    private void Jump()
+    {
+        float targetHorizontalSpeed = _moveValue.x * _jumpStrength;
+        _playerRigidBody.linearVelocity = new Vector2(targetHorizontalSpeed, 0f);
+
+        _playerRigidBody.AddForce(Vector2.up * _jumpStrength, ForceMode2D.Impulse);
+        //_playerAnimator.SetBool("Jump", true);
     }
 }
